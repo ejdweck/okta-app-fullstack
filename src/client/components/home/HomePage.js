f114d04c4d7cd00d609d1b123ed2efe049187a48
ejdweck <evanjordandweck@gmail.com>
2021-04-18 13:40:27 -0400

    :art:

--- src/client/components/home/HomePage.js
+++ src/client/components/home/HomePage.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  text-align: center;
+  padding: 60px;
+`
 
 export default class HomePage extends React.Component {
   render () {
-    return <h1>Home Page</h1>
+    return (
+      <Container>
+        <h1>Home Page</h1>
+        <h2>unsecured!</h2>
+      </Container>
+    )
   }
 }
